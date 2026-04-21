c2e1e2d1fd7ca3ad6c8545db428c7fd11613f09c
stacy <stacykerubo45@gmail.com>
2026-04-21 18:06:58 +0300

    update changes

--- script.js
+++ script.js
@@ -14,4 +14,21 @@ closeBtn.addEventListener('click', () => {
     navToggle.classList.remove('active');
 });
 
+const form = document.getElementById("contactForm");
 
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const data = {
+    name: form.name.value,
+    email: form.email.value,
+    message: form.message.value
+  };
+
+  await fetch("https://script.google.com/macros/s/AKfycbz77UTq3SuEjYSj02V1PGGcouJ_93rj22eIciO4uarzH9QmRSD7UZl7nSv1rCKRFqu8/exec", {
+    method: "POST",
+    body: JSON.stringify(data)
+  });
+
+  alert("Message sent!");
+});
